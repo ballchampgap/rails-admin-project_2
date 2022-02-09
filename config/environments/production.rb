3d741674ee7590eb126e898cef2ba48e7cad6faf
YOUR NAME <YOUR@EMAIL.com>
2022-02-10 06:36:52 +0700

    aa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,15 +117,16 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :authentication => :plain,
+  config.action_mailer.default_url_options = { host: 'admin-project-rails.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
+    :domain => 'admin-project-rails.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
     :enable_starttls_auto => true
   }
-  config.action_mailer.delivery_method = :smtp
-config.action_mailer.default_url_options ={:host => 'admin-project-rails.herokuapp.com', :protocol => 'https'}
 end

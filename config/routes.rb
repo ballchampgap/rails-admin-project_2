64552e3c9d90d70610b318fb429bf09c9a66b2cf
YOUR NAME <YOUR@EMAIL.com>
2022-02-10 10:42:55 +0700

    asd

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins, controllers: { sessions: 'admins/sessions' }
+  devise_for :admins, controllers: { sessions: 'admins/sessions', registrations: 'admins/registrations' }
   root 'admin#home'
   get 'admin' => 'admin#home'
   devise_scope :admin do

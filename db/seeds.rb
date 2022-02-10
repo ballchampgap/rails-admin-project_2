96d6abb7183fd5405a15caf38aaf3ec3b3629965
YOUR NAME <YOUR@EMAIL.com>
2022-02-10 11:01:21 +0700

    asd7

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create email: 'projectcsmju24@gmail.com', password: 'password'
+Admin.create email: 'admin@gmail.com', password: 'password'

ced3ab0a0f768f3b6b16cf77003cabbf5a8e2a9e
YOUR NAME <YOUR@EMAIL.com>
2022-02-14 04:52:00 +0700

    eswg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,16 +117,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'admin-project-rails.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'admin-project-rails.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
+  ActionMailer::Base.smtp_settings = {
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    port:     ENV['MAILGUN_SMTP_PORT'],
+    domain:   'admin-project-rails.herokuapp.com/',
+    user_name:  ENV['MAILGUN_SMTP_LOGIN'],
+    password:   ENV['MAILGUN_SMTP_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
   }
+  config.action_mailer.default_url_options = { host: "admin-project-rails.herokuapp.com"}
+  
 end

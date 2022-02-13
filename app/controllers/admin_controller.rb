e7e40df52e9e935a2fc31fb9da73978a3f700212
YOUR NAME <YOUR@EMAIL.com>
2022-02-13 21:12:04 +0700

    eastf

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,4 +2,7 @@ class AdminController < ApplicationController
   before_action :authenticate_admin!
   def home
   end
+  def read
+redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :admins, controllers: { sessions: 'admins/sessions', registrations: 'admins/registrations' }
   root 'admin#home'
-  get 'admin' => 'admin#home'
+  get '/admin' => 'admin#home'
   devise_scope :admin do
     get 'admins/sign_out' => 'devise/sessions#destroy'
     get 'admins/edit' => 'devise/registrations#edit'
     get 'admins/sign_in' => 'devise/sessions#new'
+  
   end
+get 'read' => 'admin#read'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

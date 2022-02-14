dcd65d111e4bb0a4c6201320c33ef0186a6772e0
YOUR NAME <YOUR@EMAIL.com>
2022-02-14 10:20:34 +0700

    esatge

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,16 +117,22 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
+  config.action_mailer.default_url_options = { :host => 'gmail.com' }
+
+config.active_support.deprecation = :notify
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+
+# SMTP settings 
+ActionMailer::Base.smtp_settings = {
+    :port           => 587,
+    :address        => 'smtp.gmail.com',
+    :domain         => 'gmail.com',
+    :user_name      => 'projectcsmju24@gmail.com',
+    :password       => 'project123456789',
     :authentication => :plain,
-    :user_name => Rails.application.credentials.dig(:user_name),
-    :password => Rails.application.credentials.dig(:password),
-    :domain => 'heroku.com',
-    :enable_starttls_auto => true
-  }
-  config.action_mailer.delivery_method = :smtp
-config.action_mailer.default_url_options ={:host => 'admin-managerpage.herokuapp.com', :protocol => 'https'}
+}
     
 end

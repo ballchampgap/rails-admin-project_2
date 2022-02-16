d65d34b414e3f1fbb005f697341a51f514f42fb4
YOUR NAME <YOUR@EMAIL.com>
2022-02-16 21:54:58 +0700

    wrge

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -135,7 +135,6 @@ config.action_mailer.perform_deliveries = true
 config.action_mailer.smtp_settings = {
    :address => "smtp.gmail.com",
    :port => 587,
-   :domain => 'gmail.com',
    :authentication => :plain, # I've also tried changing this to :login
    :enable_starttls_auto => true,
    :user_name => 'projectcsmju24@gmail.com',

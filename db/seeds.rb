b71ba8a81a6e52ef92b1b8a14e3f971bbaec642a
YOUR NAME <YOUR@EMAIL.com>
2022-02-17 18:54:30 +0700

    ojgr

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # Admin.create email: 'jirutsangchayza@gmail.com', password: 'password'
-Admin.create email: 'ball_080@gmail.com', password: 'password'
+Admin.create email: 'jirutsangchayza@gmail.com', password: 'password'
 # Epidemic.create(yname: "ball", plant_type: "rice", data_epidemic: "covid", lat: "18.7887002825974", lon: "98.96780650658836", description: "nocaption")
 # Epidemic.create(yname: "champ", plant_type: "cron", data_epidemic: "covid8", lat: "18.79351469159914", lon: "98.97799890080805", description: "nocaption9")
 # Pest.create(yname: "ball", plant_type: "rice", data_pest: "covid", lat: "18.7887002825974", lon: "98.96780650658836", description: "nocaption")

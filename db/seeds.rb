08f78bc1862a9dc3147e986c4d265e112bf13836
YOUR NAME <YOUR@EMAIL.com>
2022-02-24 10:30:29 +0700

    oijnpiorjhng

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create email: 'jirutsangchayza@gmail.com', password: 'password', name: 'ball'
-# Admin.create name: 'ball'
-admin = Admin.first
-admin.add_role(:super_admin)
+
+# Admin.create email: 'jirutsangchayza@gmail.com', password: 'password'
+# admin = Admin.first
+# admin.add_role(:super_admin)
+
 # Epidemic.create(yname: "ball", plant_type: "rice", data_epidemic: "covid", lat: "18.7887002825974", lon: "98.96780650658836", description: "nocaption")
 
 
@@ -20,6 +21,6 @@ admin.add_role(:super_admin)
 #     Epidemic.create(yname: "bom", plant_type: "dao", data_epidemic: "covid09", lat: "18.79351469159914", lon: "98.97799890080805", description: "nocaption8")
 # end
 
-# Pest.create(yname: "ball", plant_type: "rice", data_pest: "covid", lat: "18.7887002825974", lon: "98.96780650658836", description: "nocaption")
-# Pest.create(yname: "champ", plant_type: "cron", data_pest: "covid8", lat: "18.79351469159914", lon: "98.97799890080805", description: "nocaption9")
+Pest.create(yname: "rewg", plant_type: "rice", data_pest: "covid", lat: "18.7887002825974", lon: "98.96780650658836", description: "rwsgtrwegergrewhgerHUET5UHJET5UHTREHJRTHTRHERT")
+Epidemic.create(yname: "wegfew", plant_type: "cron", data_epidemic: "covid8", lat: "18.79351469159914", lon: "98.97799890080805", description: "erhgeryherjhrtujtyiyuoikuyioliuljklhjltuolyuiok")
 # Pest.create(yname: "bom", plant_type: "no", data_pest: "covidasf", lat: "18.7887002825974", lon: "98.96780650658836")

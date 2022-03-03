e9b664b23569726598f0716efddd269d2c7f001a
YOUR NAME <YOUR@EMAIL.com>
2022-03-03 21:50:51 +0700

    iohrg

--- app/models/complain.rb
+++
@@ -1,2 +0,0 @@
-class Complain < ApplicationRecord
-end
--- db/migrate/20220302120408_create_complain_sections.rb
+++
@@ -1,9 +0,0 @@
-class CreateComplainSections < ActiveRecord::Migration[6.1]
-  def change
-    create_table :complain_sections do |t|
-      t.string :section
-
-      t.timestamps
-    end
-  end
-end
